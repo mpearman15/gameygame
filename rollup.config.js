04283dc110fef7a9ca1226bcc2ab7f3480aeca5c
mpearman15 <mpearman@uw.edu>
2023-02-21 08:43:05 -0800

    mp3

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,3 @@
-// import { nodeResolve } from "@rollup/plugin-node-resolve";
-// import copy from "rollup-plugin-copy";
-
-// module.exports = {
-//   input: "index.js",
-//   output: {
-//     dir: "dist",
-//   },
-//   plugins: [
-//     copy({
-//       targets: [{ src: "index.html", dest: "dist" }],
-//     }),
-//     nodeResolve(),
-//   ],
-// };
-
 import copy from "rollup-plugin-copy";
 
 module.exports = {
