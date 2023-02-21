a355c780afd08db10551f01cd0742323a9dd6c33
mpearman15 <mpearman@uw.edu>
2023-02-20 18:06:31 -0800

    changes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var isGameOver;
+var score;
 var spr;
 var groundSprites;
 var GROUND_WIDTH = 50;
@@ -10,6 +12,8 @@ var switchGravity = -250;
 let colors = ["#EBA048", "#5AB3DC", "#E16540", "#B2D6BC", "#E378AA", "#CFC37D", "#C1A939", "#FF9900", "#FFCCCC", "#FDC500", "#FFD60A"];
 
 window.setup = () => {
+  isGameOver = false;
+  score = 0;
   createCanvas(600, 600);
   background(150, 200, 250);
   // creating the ground
@@ -37,49 +41,100 @@ window.setup = () => {
     ceilingSprites.add(ceilingSprite);
   }
 
-  // //creating the player
+  //creating the player
   spr = createSprite(100, height - 75, 50);
   spr.shapeColor = colors[floor(random(colors.length))];
   spr.stroke = 'none';
 
-
+  // create obstacle group
+  obstacleSprites = new Group();
 };
 
 window.draw = () => {
-  background(150, 200, 250);
-
-  if (keyboard.pressed('space')) {
-    spr.velocity.y = switchGravity;
-    switchGravity = (-1) * switchGravity;
+  if (isGameOver) {
+    background(0);
+    fill(255);
+    textAlign(CENTER);
+    text("Your score was: " + score, width/2, height/2);
+    text("Game Over! Click anywhere to restart", width/2, height/2 + 20);
+    spr.remove();
+    obstacleSprites.remove();
+    if (mouseIsPressed) {
+      isGameOver = false;
+      score = 0;
+      groundSprites.remove();
+      ceilingSprites.remove();
+      groundSprites = new Group();
+      ceilingSprites = new Group();
+      numGroundSprites = width / GROUND_WIDTH + 1;
+      for (var n = 0; n < numGroundSprites; n++) {
+        var groundSprite = createSprite(
+          n * 50,
+          height - 25,
+          GROUND_WIDTH,
+          GROUND_HEIGHT
+        )
+        groundSprite.shapeColor = colors[floor(random(colors.length))];
+        groundSprite.stroke = 'none';
+        groundSprites.add(groundSprite);
+        var ceilingSprite = createSprite(
+          n * 50,
+          25,
+          GROUND_WIDTH,
+          GROUND_HEIGHT
+        )
+        ceilingSprite.shapeColor = colors[floor(random(colors.length))];
+        ceilingSprite.stroke = 'none';
+        ceilingSprites.add(ceilingSprite);
+      }
+      //creating the player
+      spr = createSprite(100, height - 75, 50);
+      spr.shapeColor = colors[floor(random(colors.length))];
+      spr.stroke = 'none';
+      // create obstacle group
+      obstacleSprites = new Group();
+    }
+  } else {
+    background(150, 200, 250);
+    if (keyboard.pressed('space')) {
+      score = score + 1;
+      spr.velocity.y = switchGravity;
+      switchGravity = (-1) * switchGravity;
+    }
+    if (spr.overlap(ceilingSprites)) {
+      spr.velocity.y = 0;
+      spr.position.y = 50 + 25;
+    } else if (spr.overlap(groundSprites)) {
+      spr.velocity.y = 0;
+      spr.position.y = height - 50 - 25;
+    }
+    spr.position.x = spr.position.x + 5;
+    camera.x = spr.position.x + width / 4;
+    var firstGroundSprite = groundSprites[0];
+    var firstCeilingSprite = ceilingSprites[0];
+    if (firstGroundSprite.position.x <=
+      camera.x - (width / 2 + firstGroundSprite.width / 2)) {
+      groundSprites.remove(firstGroundSprite);
+      ceilingSprites.remove(firstCeilingSprite);
+      firstGroundSprite.position.x = firstGroundSprite.position.x + numGroundSprites * firstGroundSprite.width;
+      firstCeilingSprite.position.x = firstCeilingSprite.position.x + numGroundSprites * firstCeilingSprite.width;
+      groundSprites.add(firstGroundSprite);
+      ceilingSprites.add(firstCeilingSprite);
+    }
+    if (random() > 0.97) {
+      var obstacle = createSprite(camera.x + width, random(0, (height-50)-10), 30);
+      obstacleSprites.add(obstacle);
+    }
+    var firstObstacle = obstacleSprites[0];
+    if (obstacleSprites.length > 0 && firstObstacle.position.x <= camera.x - (width/2 + firstObstacle.width/2)) {
+        obstacleSprites.remove(firstObstacle);
+    }
+    for (var i = 0; i < obstacleSprites.length; i++) {
+      // obstacleSprites[i].position.x -= 5;
+      obstacleSprites[i].overlap(spr, endGame);
+    }
   }
-
-  if (spr.overlap(ceilingSprites)) { // this keeps the ceiling things not from doing that cool breaking thing but currently it just disappears forever into the void
-    spr.velocity.y = 0;
-    spr.position.y = 50 + 25;
-  } else if (spr.overlap(groundSprites)) {
-    spr.velocity.y = 0;
-    spr.position.y = height - 50 - 25;
-  }
-
-
-  spr.position.x = spr.position.x + 3;
-  camera.position.x = spr.position.x + width / 4;
-  var firstGroundSprite = groundSprites[0];
-  var firstCeilingSprite = ceilingSprites[0];
-  if (firstGroundSprite.position.x <=
-    camera.position.x - (width / 2 + firstGroundSprite.width / 2)) {
-      // console.log("before removing: " + groundSprites[numGroundSprites-1]);
-    groundSprites.remove(firstGroundSprite);
-    // console.log("after removing: " + groundSprites[numGroundSprites-1]);
-    ceilingSprites.remove(firstCeilingSprite);
-    firstGroundSprite.position.x = firstGroundSprite.position.x + numGroundSprites * firstGroundSprite.width;
-    // console.log(groundSprites[numGroundSprites-1]);
-    groundSprites.add(firstGroundSprite);
-    // spr.shapeColor = 'yellow';
-    // console.log(firstGroundSprite.color.levels);
-    ceilingSprites.add(firstCeilingSprite);
-  }
-  // if the sprite is not currently in that space (position.y = whatever), and the colors align, then you get a plus point
-  // how do i get a grasp of what the indexing is for each thing generated? can i reference the id number?
-
 };
+function endGame() {
+  isGameOver = true;
+}
